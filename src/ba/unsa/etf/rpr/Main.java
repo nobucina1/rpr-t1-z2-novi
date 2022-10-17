4fe84ebeae34a7c13a1c167ac42ebc2bfa2553e7
nobucina <nobucina1@etf.unsa.ba>
2022-10-17 13:16:39 +0200

    Initial commit

---
+++ src/ba/unsa/etf/rpr/Main.java
@@ -0,0 +1,21 @@
+package ba.unsa.etf.rpr;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        System.out.println("Unesi n: ");
+        int n = in.nextInt();
+        for (int i=1; i<n; i++) {
+            if (i % sumaCifara(i) == 0)
+                System.out.println(i);
+        }
+    }
+    public static int sumaCifara(int x) {
+        int suma = 0;
+        for (int i=x; i!=0; i/=10)
+            suma += i%10;
+        return suma;
+    }
+}
